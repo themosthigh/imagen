62cc43cdffaecb37072d86655d9fe1ec447b5617
TheMostHigh <tallestlegacy@gmail.com>
2026-02-20 21:17:00 +0300

    feat: Resize based on terminal width

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-use image::{DynamicImage, GenericImageView, ImageReader};
+mod my_image;
+
+use my_image::{draw, load_image, resize};
 
 const IMAGE_SRC: &str = "./inputs/shawn.png";
-const ASCII_RAMP: &str = ".:-=+*#%@";
 
 fn main() {
-    let img = ImageReader::open(IMAGE_SRC).unwrap().decode().unwrap();
-    let new_width = 100;
+    // load image from disk
+    let img = load_image(IMAGE_SRC);
 
-    let resized = resize(img, new_width);
-    let width = resized.width();
+    // resize image to terminal width
+    let (term_w, _term_h) = crossterm::terminal::size().unwrap();
+    let resized = resize(img, term_w as u32);
 
-    for (x, _y, pixel) in resized.pixels() {
-        let [r, g, b, _a] = pixel.0;
-
-        // Gemini: luminance standard formula
-        let brightess = (r as f32 * 0.2126 + g as f32 * 0.7152 + b as f32 * 0.0722) as u8;
-        let ascii = map_to_ascii(brightess);
-        print!("{}", ascii);
-        if x == width - 1 {
-            println!();
-        }
-    }
-}
-
-// Gemini: map a lightness value to an ASCII character
-fn map_to_ascii(lightness: u8) -> char {
-    // Ordered from darkest (space) to lightest (density)
-
-    // Scale 0-255 to 0-(ramp_length - 1)
-    let index = (lightness as f32 / 255.0 * (ASCII_RAMP.len() - 1) as f32).round() as usize;
-
-    ASCII_RAMP.as_bytes()[index] as char
-}
-
-fn resize(img: DynamicImage, new_width: u32) -> DynamicImage {
-    let ratio = new_width as f32 / img.width() as f32;
-    let new_height = img.height() as f32 * ratio;
-
-    img.resize_exact(
-        new_width,
-        (new_height * 0.5) as u32,
-        image::imageops::FilterType::Lanczos3,
-    )
+    // draw image to terminal
+    draw(resized);
 }
---
+++ src/my_image.rs
@@ -0,0 +1,43 @@
+use image::{DynamicImage, GenericImageView, ImageReader};
+
+const ASCII_RAMP: &str = ".:-=+*#%@";
+
+pub fn load_image(path: &str) -> DynamicImage {
+    ImageReader::open(path).unwrap().decode().unwrap()
+}
+
+// Gemini: map a lightness value to an ASCII character
+fn map_to_ascii(lightness: u8) -> char {
+    // Ordered from darkest (space) to lightest (density)
+
+    // Scale 0-255 to 0-(ramp_length - 1)
+    let index = (lightness as f32 / 255.0 * (ASCII_RAMP.len() - 1) as f32).round() as usize;
+
+    ASCII_RAMP.as_bytes()[index] as char
+}
+
+pub fn resize(img: DynamicImage, new_width: u32) -> DynamicImage {
+    let ratio = new_width as f32 / img.width() as f32;
+    let new_height = img.height() as f32 * ratio;
+
+    img.resize_exact(
+        new_width,
+        (new_height * 0.5) as u32,
+        image::imageops::FilterType::Lanczos3,
+    )
+}
+
+pub fn draw(img: DynamicImage) {
+    let width = img.width();
+    for (x, _y, pixel) in img.pixels() {
+        let [r, g, b, _a] = pixel.0;
+
+        // Gemini: luminance standard formula
+        let brightess = (r as f32 * 0.2126 + g as f32 * 0.7152 + b as f32 * 0.0722) as u8;
+        let ascii = map_to_ascii(brightess);
+        print!("{}", ascii);
+        if x == width - 1 {
+            println!();
+        }
+    }
+}
